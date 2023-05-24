c6aa590df0fee1a2dd7f434e427641a80e3d566f
Tracer Yong <yongtracer@core-login2.cluster.france-bioinformatique.fr>
2023-05-24 21:53:18 +0200

    create bash script

---
+++ scripts/yongtracer_read_QC.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "script start: download and initial sequencing read quality control"
+date
+######commands start##########
+
+
+
+
+
+
+#####commands end###########
+date
+echo "script end."
+
