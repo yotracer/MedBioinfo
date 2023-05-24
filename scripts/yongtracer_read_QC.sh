e0e04bef0523a0e5d3510ffbd25727231ae3de52
Tracer Yong <yongtracer@core-login2.cluster.france-bioinformatique.fr>
2023-05-24 22:55:47 +0200

    extract run accession

--- scripts/yongtracer_read_QC.sh
+++ scripts/yongtracer_read_QC.sh
@@ -2,7 +2,8 @@
 echo "script start: download and initial sequencing read quality control"
 date
 ######commands start##########
-
+sqlite3 -noheader -csv /shared/home/yongtracer/medbioinfo_folder/pascal/central_database/sample_collab.db "SELECT run_accession FROM sample_annot LEFT JOIN sample2bioinformatician ON sample_annot.patient_code = sample2bioinformatician.patient_code WHERE username = 'yongtracer'; " > /shared/home/yongtracer/medbioinfo_folder/tracer/MedBioinfo/analyses/yongtracer_run_accessions.txt
+mkdir ../data/sra_fastq
 
 
 
