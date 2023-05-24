9baff023bd94ab5ebd2fc5b1b8aff00706dd35ac
Tracer Yong <yongtracer@core-login2.cluster.france-bioinformatique.fr>
2023-05-25 00:02:04 +0200

    add fastq-dump

--- scripts/yongtracer_read_QC.sh
+++ scripts/yongtracer_read_QC.sh
@@ -4,7 +4,8 @@ date
 ######commands start##########
 sqlite3 -noheader -csv /shared/home/yongtracer/medbioinfo_folder/pascal/central_database/sample_collab.db "SELECT run_accession FROM sample_annot LEFT JOIN sample2bioinformatician ON sample_annot.patient_code = sample2bioinformatician.patient_code WHERE username = 'yongtracer'; " > /shared/home/yongtracer/medbioinfo_folder/tracer/MedBioinfo/analyses/yongtracer_run_accessions.txt
 mkdir ../data/sra_fastq
-
+module load sra-tools
+cat yongtracer_run_accessions.txt | sun —cpus-per-task=1 —time=00:3000 xargs fastq-dump --split-e --gzip --readids --outdir ../data/sra_fastq/ --disable-multithreading
 
 
 
