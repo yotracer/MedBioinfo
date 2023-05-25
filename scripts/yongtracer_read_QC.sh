2f0f6240755909a6d203bb6af2476fbaafb60678
Tracer Yong <yongtracer@core-login2.cluster.france-bioinformatique.fr>
2023-05-25 12:57:59 +0200

    start fastqc part

--- scripts/yongtracer_read_QC.sh
+++ scripts/yongtracer_read_QC.sh
@@ -2,10 +2,17 @@
 echo "script start: download and initial sequencing read quality control"
 date
 ######commands start##########
+# extract accession number by left join sample_annot with sample2bioinformatician for myself
 sqlite3 -noheader -csv /shared/home/yongtracer/medbioinfo_folder/pascal/central_database/sample_collab.db "SELECT run_accession FROM sample_annot LEFT JOIN sample2bioinformatician ON sample_annot.patient_code = sample2bioinformatician.patient_code WHERE username = 'yongtracer'; " > /shared/home/yongtracer/medbioinfo_folder/tracer/MedBioinfo/analyses/yongtracer_run_accessions.txt
 mkdir ../data/sra_fastq
 module load sra-tools
-cat yongtracer_run_accessions.txt | sun —cpus-per-task=1 —time=00:3000 xargs fastq-dump --split-e --gzip --readids --outdir ../data/sra_fastq/ --disable-multithreading
+
+# Download FASTQ data according to extracted accession number
+cat yongtracer_run_accessions.txt | srun --cpus-per-task=1 --time=00:3000 xargs fastq-dump --split-e --gzip --readids --outdir ../data/sra_fastq/ --disable-multithreading
+
+# Quality contral with fastQC
+mkdir /shared/home/yongtracer/medbioinfo_folder/tracer/MedBioinfo/analyses/fastqc
+module load fastqc
 
 
 
