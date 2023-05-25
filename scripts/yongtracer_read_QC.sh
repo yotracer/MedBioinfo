ab3a573dd93b6253796a606b7ec4ae1f064ae6f4
Tracer Yong <yongtracer@core-login2.cluster.france-bioinformatique.fr>
2023-05-25 13:28:40 +0200

    finish QC, start moving file section

--- scripts/yongtracer_read_QC.sh
+++ scripts/yongtracer_read_QC.sh
@@ -13,7 +13,10 @@ cat yongtracer_run_accessions.txt | srun --cpus-per-task=1 --time=00:3000 xargs 
 # Quality contral with fastQC
 mkdir /shared/home/yongtracer/medbioinfo_folder/tracer/MedBioinfo/analyses/fastqc
 module load fastqc
+srun --cpus-per-task=2 --time=00:10:00 xargs -I{} -a yongtracer_run_accessions.txt fastqc --outdir ./fastqc/ --threads 2 --noextract \
+../data/sra_fastq/{}_1.fastq.gz ../data/sra_fastq/{}_2.fastq.gz
 
+# Moving files from remote server to local disk
 
 
 
