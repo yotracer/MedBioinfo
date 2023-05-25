b778b6ba98dc9e5c8b1e1d2624a41fd28b0df663
Tracer Yong <yongtracer@core-login2.cluster.france-bioinformatique.fr>
2023-05-25 14:48:21 +0200

    merge pair-end section add

--- scripts/yongtracer_read_QC.sh
+++ scripts/yongtracer_read_QC.sh
@@ -13,11 +13,16 @@ cat yongtracer_run_accessions.txt | srun --cpus-per-task=1 --time=00:3000 xargs 
 # Quality contral with fastQC
 mkdir /shared/home/yongtracer/medbioinfo_folder/tracer/MedBioinfo/analyses/fastqc
 module load fastqc
-srun --cpus-per-task=2 --time=00:10:00 xargs -I{} -a yongtracer_run_accessions.txt fastqc --outdir ./fastqc/ --threads 2 --noextract \
-../data/sra_fastq/{}_1.fastq.gz ../data/sra_fastq/{}_2.fastq.gz
+srun --cpus-per-task=2 --time=00:10:00 xargs -I{} -a yongtracer_run_accessions.txt fastqc --outdir ./fastqc/ --threads 2 --noextract ../data/sra_fastq/{}_1.fastq.gz ../data/sra_fastq/{}_2.fastq.gz
 
 # Moving files from remote server to local disk
+scp yongtracer@core.cluster.france-bioinformatique.fr:/shared/projects/2314_medbioinfo/tracer/MedBioinfo/analyses/fastqc/*.html ~/Downloads
 
+# Merging paired end reads
+module load flash2
+mkdir /shared/projects/2314_medbioinfo/tracer/MedBioinfo/data/merged_pairs
+srun --cpus-per-task=2 xargs I{} -a yongtracer_run_accessions.txt flash2 --threads=2 -z --output-directory=../data/merged_pairs/ --output-prefix={}.flash /shared/projects/2314_medbioinfo/tracer/MedBioinfo/data/sra_fastq/{}_1.fastq.gz /shared/projects/2314_medbioinfo/tracer/MedBioinfo/data/sra_fastq/{}_2.fastq.gz 2>&1 | tee -a yongtracer_flash2.log
+                                                      
 
 
 #####commands end###########
